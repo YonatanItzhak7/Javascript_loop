a9ecf020d0d0ffc47c1af57a8d0641c70c39049f
Yonatanitzhak7 <Yonatan1171@gmail.com>
2022-06-21 18:46:48 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+// let myObj = {
+//     age: 20,
+//     firstname:"Yonatan",
+//     lastname:"Itzhak"
+// }
+// let myObj2 = {
+//     age: 12,
+//     firstname:"Eldad",
+//     lastname:"Brhano"
+// }
+// let myArray = [myObj,myObj2]
+// myArray.forEach(printObj=>{
+//     if(printObj.age>18){
+//         h1_id.innerHTML += "User first name:"+" "+printObj.firstname+"<br>"
+//     }
+//     else{
+//         h1_id.innerHTML += "User last name:"+" "+printObj.lastname+"<br>"
+//     }
+// })
+// function changeColor(){
+// let myElem = document.getElementsByClassName("sameElements")
+// for(elementItem of myElem){
+//     console.log(elementItem);
+//     elementItem.style.color = "blue"
+//     elementItem.innerHTML = "Hello"
+// }
+// }
+
+// let myObj ={
+//     name: "Yonatan",
+//     age:25,
+//     city:"Rishon-Lezyion",
+//     favcolor:"red"
+// }
+
+// for(let key in myObj){
+//     console.log(`${key} : ${myObj[key]}`);
+//     document.body.innerHTML += `<br><div> ${key}: ${myObj[key]} </div>`
+// }
+
+// function objCreator(){
+//     let myInput = document.getElementsByClassName("inputUser");
+//     let userObj = {
+//         firstname: myInput,
+//         lastname:myInput,
+//         age:myInput
+//     }
+//     for(let myInput in userObj){
+//         document.body.innerHTML += `<br><div> ${myInput}: ${userObj[myInput]} </div>`
+//     }
+// }
